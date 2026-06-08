01136467c1c62cd4975253411aab0528d193f489
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2026-06-08 22:56:27 +0300

    Using 'reduce()' with sparse arrays

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-const pipe =
-  (...functions) =>
-  (initialValue) =>
-    functions.reduce((acc, fn) => fn(acc), initialValue);
+console.log([1, 2, , 4].reduce((a, b) => a + b));
+console.log([1, 2, undefined, 4].reduce((a, b) => a + b));
 
-const double = (x) => 2 * x;
-const triple = (x) => 3 * x;
-const quadruple = (x) => 4 * x;
-
-const multiply6 = pipe(double, triple);
-const multiply9 = pipe(triple, triple);
-const multiply16 = pipe(quadruple, quadruple);
-const multiply24 = pipe(double, triple, quadruple);
-
-console.log(multiply6(6));
-console.log(multiply9(9));
-console.log(multiply16(16));
-console.log(multiply24(10));
+// Calling reduce() on non-array objects -- MDN
