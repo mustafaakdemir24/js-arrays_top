36aedc50be2959676062a1cbbb10905b56baf986
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2026-06-10 16:16:51 +0300

    Defining composable functions.

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-const sum = [0, 1, 2, 3].reduceRight((a, b) => a + b);
+const compose =
+  (...args) =>
+  (value) =>
+    args.reduceRight((acc, fn) => fn(acc), value);
 
-console.log(sum);
+const inc = (n) => n + 1;
+
+const double = (n) => n * 2;
+
+console.log(compose(double, inc)(2));
+
+console.log(compose(inc, double)(2));
