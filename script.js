b21b9a1c3f97e9d3de23a34ab39f1f10012feeb9
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2026-06-10 21:24:29 +0300

    Find odd numbers and output as a new array by using 'filter()' array method.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const isOdd = (num) => num % 2 !== 0;
 const arr = [1, 2, 3, 4, 5];
-const mappedArr = arr.map((num) => num + 1);
-console.log(mappedArr); // Output: [2, 3, 4, 5, 6]
+const oddNums = arr.filter(isOdd);
+console.log(oddNums); // Output: [1, 3, 5]
+console.log(arr); // Output: [1, 2, 3, 4, 5]
