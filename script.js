cf74accf7a83f511799cc82c0bd907d08d341a78
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2026-06-10 21:03:45 +0300

    Increase an array elements by one and retun new array by useing 'arr.map()' method.

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
-const sumOfTripledEvens = (array) => {
-  let sum = 0;
-  for (let i = 0; i < array.length; i++) {
-    // Step 1: If the element is an even number
-    if (array[i] % 2 === 0) {
-      // Step 2: Multiply this number by three
-      const tripledEvenNumber = array[i] * 3;
-
-      // Step 3: Add the new number to the total
-      sum += tripledEvenNumber;
-    }
-  }
-  return sum;
+const addOne = (num) => {
+  return num + 1;
 };
+
+const arr = [1, 2, 3, 4, 5];
+const mappedArr = arr.map(addOne);
+console.log(mappedArr); // Output: [2, 3, 4, 5, 6]
