015cd57e6ac9bf73f12df33343e38498ff5986f1
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2026-06-10 16:21:49 +0300

    Use 'reduceRight()' with sparse arrays.

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-const compose =
-  (...args) =>
-  (value) =>
-    args.reduceRight((acc, fn) => fn(acc), value);
+console.log([1, 2, , 4].reduceRight((a, b) => a + b));
 
-const inc = (n) => n + 1;
-
-const double = (n) => n * 2;
-
-console.log(compose(double, inc)(2));
-
-console.log(compose(inc, double)(2));
+console.log([1, 2, undefined, 4].reduceRight((a, b) => a + b));
