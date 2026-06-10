cd53d4a917434b5f0681b1c2480c9a47b1591657
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2026-06-10 21:10:32 +0300

    More elegant way to implement increasing array element by one usung 'map()' method.

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-const addOne = (num) => {
-  return num + 1;
-};
-
 const arr = [1, 2, 3, 4, 5];
-const mappedArr = arr.map(addOne);
+const mappedArr = arr.map((num) => num + 1);
 console.log(mappedArr); // Output: [2, 3, 4, 5, 6]
