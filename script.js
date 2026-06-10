b4fc03f380c170fe86a3bc1b8eb202a949e888d2
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2026-06-10 21:29:07 +0300

    More elegant way to find odd numbers adn output them as a new arrey by using 'filter()' method.

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const isOdd = (num) => num % 2 !== 0;
 const arr = [1, 2, 3, 4, 5];
-const oddNums = arr.filter(isOdd);
+const oddNums = arr.filter((num) => num % 2 !== 0);
 console.log(oddNums); // Output: [1, 3, 5]
 console.log(arr); // Output: [1, 2, 3, 4, 5]
