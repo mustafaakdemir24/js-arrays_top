7a8956c1782f13a03db73ceb9889d6143009d8d7
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2026-06-11 21:27:14 +0300

    Filter range 'in place' example solution -- javascript.info

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-const filterRange = (arr, a, b) => {
-  return arr.filter((num) => num >= a && num <= b);
+const filterRangeInPlace = (arr, a, b) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < a || arr[i] > b) {
+      arr.splice(i, 1);
+      i--;
+    }
+  }
+  return arr;
 };
 
 let arr = [5, 3, 8, 1];
 
-console.log(filterRange(arr, 1, 4));
+console.log(filterRangeInPlace(arr, 1, 4));
+
 console.log(arr);
