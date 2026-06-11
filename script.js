dac60636f284ad28f9e1841955ae6eef2d379d5d
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2026-06-11 23:03:45 +0300

    'Suffle an array' example solution -- javascript.inof

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-let arr = [5, 2, 1, -10, 8];
+// Fisher - Yates (Lmitj) algorithm
+const suffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
 
-arr.sort((a, b) => b - a);
+let arr = new Array(1, 2, 3);
 
-console.log(arr);
+console.log(suffleArray(arr));
+console.log(suffleArray(arr));
+console.log(suffleArray(arr));
+
+// Assignment -- Filter unique array members --> The Odin Project
